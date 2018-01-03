3c68c0e7adfcd96e902db982c30ef71d27665655
Liu Wei <liuweiky@qq.com>
2018-01-03 15:05:12 +0800

    更新地址

--- js/docu.js
+++ js/docu.js
@@ -13,7 +13,8 @@ var config = {
 window.onload = function () {
     app = new t3d.App({
         el: "div3d",
-        url: "https://speech.uinnova.com/static/models/NARA",
+        // url: "https://speech.uinnova.com/static/models/NARA",
+        url: "http://uinnova-model.oss-cn-beijing.aliyuncs.com/scenes/NARA",
         ak:'app_test_key',
         complete: function () {
             console.log("app scene loaded");
